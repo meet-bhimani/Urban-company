2ba5d493e959fd06e757153669d1ce5123d33d91
meet-bhimani <meet.bhimani@bacancy.com>
2024-04-01 19:04:07 +0530

    [FEAT]: created common helmet header component for using dynamic title and description for different of pages

---
+++ client/src/components/common/HelmetHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Helmet } from 'react-helmet-async'
+
+const HelmetHeader = ({ title, description }) => {
+  return (
+    <>
+      <Helmet>
+        <title title>{title}</title>
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
+      </Helmet>
+    </>
+  )
+}
+
+export default HelmetHeader
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,15 @@ import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
 import App from './App.jsx'
 import { ThemeProvider } from './context/themeContext.jsx'
+import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <Provider store={store}>
-        <App />
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
