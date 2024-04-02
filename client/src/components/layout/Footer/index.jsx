895e564c08aa25eae4e23507a61dbee6a3fe80fa
meet-bhimani <meet.bhimani@bacancy.com>
2024-04-02 15:23:00 +0530

    [FEAT]: added footer text

--- client/src/components/layout/Footer/index.jsx
+++ client/src/components/layout/Footer/index.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <h1>Footer</h1>
+    <div className="py-2 w-[90%] mx-auto">
+      <p className="text-center text-xs sm:text-sm flex flex-col xsm:flex-row items-center justify-center gap-2">
+        &copy; Copyright {new Date().getFullYear()} Urban Company |<span>All rights reserved</span>
+      </p>
     </div>
   )
 }
